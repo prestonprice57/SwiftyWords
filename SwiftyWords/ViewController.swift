3be012c26dc19131cfebedf3eb8e709bddd00c45
Preston Price <pwprice57@gmail.com>
2015-12-08 22:06:28 -0700

    First commit

--- SwiftyWords/ViewController.swift
+++ SwiftyWords/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var answersLabel: UILabel!
+    @IBOutlet weak var cluesLabel: UILabel!
+    @IBOutlet weak var currentAnswer: UITextField!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func submitTapped(sender: AnyObject) {
+    }
+
+    @IBAction func clearTapped(sender: AnyObject) {
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
